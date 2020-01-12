360b95b8512c5e2a586581a5e04e402d1e0b9cbf
Faisal Younus <gitfyhub>
2020-01-12 14:41:11 -0600

    initial

---
+++ index.js
@@ -0,0 +1,88 @@
+// console.clear();
+
+// People dropping off a form
+const createPolicy = (name, amount) => {
+    return {
+      type: "CREATE_POLICY",
+      payload: {
+        name: name,
+        amount: amount
+      }
+    };
+  };
+  
+  // People deleting a policy
+  const deletePolicy = name => {
+    return {
+      type: "DELETE_POLICY",
+      payload: {
+        name: name
+      }
+    };
+  };
+  
+  // Create claim
+  const createClaim = (name, amountOfMoneyToCollect) => {
+    return {
+      type: "CREATE_CLAIM",
+      payload: {
+        name: name,
+        amountOfMoneyToCollect: amountOfMoneyToCollect
+      }
+    };
+  };
+  
+  // Reducers (Departments)
+  const claimsHistory = (oldListOfClaims = [], action) => {
+    if (action.type === "CREATE_CLAIM") {
+      // care about the action
+      // creates a new array
+      return [...oldListOfClaims, action.paylaod];
+    }
+    // dont care about the action
+    return oldListOfClaims;
+  };
+  
+  const accounting = (bagOfMoney = 100, action) => {
+    if (action.type === "CREATE_CLAIM") {
+      return bagOfMoney - action.payload.amountOfMoneyToCollect;
+    } else if (action.type === "CREATE_POLICY") {
+      return bagOfMoney + action.payload.amount;
+    }
+    return bagOfMoney;
+  };
+  
+  const policies = (listOfPolicies = [], action) => {
+    if (action.type === "CREATE_POLICY") {
+      return [...listOfPolicies, action.payload.name];
+    } else if (action.type === "DELETE_POLICY") {
+      return listOfPolicies.filter(name => name !== action.payload.name);
+    }
+    return listOfPolicies;
+  };
+  
+  //console.log(Redux);
+  
+  const { createStore, combineReducers } = Redux;
+  
+  const ourDepartments = combineReducers({
+    moneyWeHave: accounting,
+    claimsHistory: claimsHistory,
+    policies: policies
+  });
+  
+  const store = createStore(ourDepartments);
+  
+  // const action = createPolicy('Alex', 20)
+  // store.dispatch(action);
+  
+  store.dispatch(createPolicy('Alex', 20));
+  store.dispatch(createPolicy('Jim', 30));
+  store.dispatch(createPolicy('Bob', 40));
+  
+  store.dispatch(createClaim('Alex', 120));
+  store.dispatch(createClaim('Jim', 50));
+  
+  store.dispatch(deletePolicy('Bob'));
+  
+  console.log(store.getState());
